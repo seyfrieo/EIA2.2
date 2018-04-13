505ff0bcc868d917c9ba955389aaa0a944f6bc06
Dominik Seyfried <32566091+seyfrieo@users.noreply.github.com>
2018-04-13 22:49:56 +0200

    Add files via upload

---
+++ Aufgabe 2/memory.js
@@ -0,0 +1,5 @@
+var Memory;
+(function (Memory) {
+    document.addEventListener("DOMContentLoaded", main);
+})(Memory || (Memory = {}));
+//# sourceMappingURL=memory.js.map
---
+++ Aufgabe 2/memory.ts
@@ -0,0 +1,5 @@
+namespace Memory {
+
+
+  document.addEventListener("DOMContentLoaded", main);
+}
