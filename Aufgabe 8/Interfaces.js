3bf64fcfb292ca9dbe4373d22a699f6d446679b1
Dominik Seyfried <32566091+seyfrieo@users.noreply.github.com>
2018-06-10 18:07:20 +0200

    Add files via upload

---
+++ Aufgabe 8/Interfaces.js
@@ -0,0 +1,15 @@
+/*    Aufgabe: (Aufgabe 6)
+    Name: (Dominik Seyfried)
+    Matrikel: (256734)
+    Datum: (27.05.2018)
+    Hiermit versichere ich, dass ich diesen
+    Code selbst geschrieben habe. Er wurde
+    nicht kopiert und auch nicht diktiert.*/
+var A6;
+(function (A6) {
+    // Simples Array zum Speichern der Studi-Datensätze (nur zur Demonstration)
+    A6.studiSimpleArray = [];
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    A6.studiHomoAssoc = {};
+})(A6 || (A6 = {}));
+//# sourceMappingURL=Interfaces.js.map
